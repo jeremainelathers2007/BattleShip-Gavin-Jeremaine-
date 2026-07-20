493ba9da1c2b0824ad42c8a8e51e92baec93027f
Lathers <1364226469157002@mil>
2026-07-20 09:50:35 -0400

    Board

--- Battleship.py
+++ Battleship.py
@@ -1,6 +1,18 @@
 import random
+def battleship_coordinates(grid):
+    row1 = grid[0]
+    row2 = grid[1]
+    row3 = grid[2]
+    row4 = grid[3]
 
 if __name__ == "__main__":
+    print("-- Welcome To BattleShip --")
+    rows = [1,2,3,4]
+    columns = ["A","B","C","D"]
+    grid = [["0" for _ in range(len(columns))] for _ in range(len(rows))]
+    print("  " + " ".join(columns))
+    for i, row_data in enumerate(grid):
+        print(f"{rows[i]} " + " ".join(row_data))
     columns = ["a", "b", "c", "d"]
     shipRow = random.randint(1, 4)
     shipColumn = random.choice(columns)
@@ -21,5 +33,6 @@ if __name__ == "__main__":
     
     print(f"{columnGuess}, {rowGuess}")
     print(f"{shipColumn}, {shipRow}")
+    
 
         
