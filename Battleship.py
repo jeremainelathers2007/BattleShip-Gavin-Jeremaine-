144eea705a1edeb42af3c77b8aac684d0b6fadb6
Lathers <1364226469157002@mil>
2026-07-20 13:51:11 -0400

    VariousBoards

--- Battleship.py
+++ Battleship.py
@@ -19,6 +19,7 @@ if __name__ == "__main__":
     print(f"Starting Board: \n{board[1]} \n{board[2]} \n{board[3]} \n{board[4]}")
 
     while True:
+        columnNum = 0
         attempts += 1
         print(f"\nAttempt #{attempts}")
         while True:
@@ -31,29 +32,32 @@ if __name__ == "__main__":
             while True:
                 columnGuess = input("Please enter a column(a, b, c, d)").strip() .lower()
                 if columnGuess == "a":
-                    columnNum = 0
+                    columnNum += 0
                 if columnGuess == "b":
-                    columnNum = 1
+                    columnNum += 1
                 if columnGuess == "c":
-                    columnNum = 2
+                    columnNum += 2
                 if columnGuess == "d":
-                    columnNum = 3
+                    columnNum += 3
                 elif columnGuess == "a" or columnGuess == "b" or columnGuess == "c" or columnGuess == "d":
                     break
                 else:
                     print("Please enter one of the following: a, b, c, d")
             if (rowGuess, columnGuess) not in playerGuesses:
                 playerGuesses.append((rowGuess, columnGuess))
-                board[rowGuess][columnNum] + 1
+                board[rowGuess][columnNum] += 1
                 print(f"Updated Board: \n{board[1]} \n{board[2]} \n{board[3]} \n{board[4]}")
                 break
             else:
                 print("Coordinate already guessed, try again")
 
         if (rowGuess, columnGuess) == shipCoord:
+            board[rowGuess][columnNum] += 1
             print(f"Final Board: \n{board[1]} \n{board[2]} \n{board[3]} \n{board[4]}")
             print("Congrats you sunk the ship!")
             break
+        else:
+            print("You missed! Try Again")
 
     
 
