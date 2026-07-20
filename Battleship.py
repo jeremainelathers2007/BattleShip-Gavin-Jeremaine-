072325702a59869c3cea6f20c1c4d46c4297e82b
griezinger <gavinriezinger08@gmail.com>
2026-07-20 10:43:12 -0400

    finalized the game loop aside from updating the board

--- Battleship.py
+++ Battleship.py
@@ -1,38 +1,40 @@
 import random
-def battleship_coordinates(grid):
-    row1 = grid[0]
-    row2 = grid[1]
-    row3 = grid[2]
-    row4 = grid[3]
 
 if __name__ == "__main__":
-    print("-- Welcome To BattleShip --")
-    rows = [1,2,3,4]
-    columns = ["A","B","C","D"]
-    grid = [["0" for _ in range(len(columns))] for _ in range(len(rows))]
-    print("  " + " ".join(columns))
-    for i, row_data in enumerate(grid):
-        print(f"{rows[i]} " + " ".join(row_data))
     columns = ["a", "b", "c", "d"]
     shipRow = random.randint(1, 4)
     shipColumn = random.choice(columns)
-
+    shipCoord = (shipRow, shipColumn)
+    print(shipCoord)
+    playerGuesses = []
+    attempts = 0
 
     while True:
-        try:
-            rowGuess = int(input("\nPlease enter a row(1-4): "))
+        attempts += 1
+        print(f"\nAttempt #{attempts}")
+        while True:
+            while True:
+                try:
+                    rowGuess = int(input("Please enter a row(1-4): "))
+                    break
+                except:
+                    print("Enter a number 1-4")
+            while True:
+                columnGuess = input("Please enter a column(a, b, c, d)").strip() .lower()
+                if columnGuess == "a" or columnGuess == "b" or columnGuess == "c" or columnGuess == "d":
+                    break
+                else:
+                    print("Please enter one of the following: a, b, c, d")
+            if (rowGuess, columnGuess) not in playerGuesses:
+                playerGuesses.append((rowGuess, columnGuess))
+                break
+            else:
+                print("Coordinate already guessed, try again")
+
+        if (rowGuess, columnGuess) == shipCoord:
+            print("Congrats you sunk the ship!")
             break
-        except:
-            print("Enter a number 1-4")
-    while True:
-        columnGuess = input("\nPlease enter a column(a, b, c, d)").strip() .lower()
-        if columnGuess == "a" or columnGuess == "b" or columnGuess == "c" or columnGuess == "d":
-            break
-        else:
-            print("Please enter one of the following: a, b, c, d")
-    
-    print(f"{columnGuess}, {rowGuess}")
-    print(f"{shipColumn}, {shipRow}")
+
     
 
         
