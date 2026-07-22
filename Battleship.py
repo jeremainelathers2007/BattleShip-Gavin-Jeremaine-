eb01b7d3e666f1650f0ef8fe57232907325c78c8
griezinger <gavinriezinger08@gmail.com>
2026-07-22 07:48:36 -0400

    replaced list of letters

--- Battleship.py
+++ Battleship.py
@@ -15,10 +15,10 @@ if __name__ == "__main__":
         rows.append(rowCounter)
         rowCounter+=1
 
-    letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+    numberToLetter = {0:"a", 1:"b",2:"c", 3:"d", 4:"e", 5:"f", 6:"g", 7:"h", 8:"i", 9:"j"}
     usableLetters = []
     for e in range(boardSize):
-        usableLetters.append(letters[e])
+        usableLetters.append(numberToLetter[e])
     
     playerBoard = {}
     boardCounter = 1
